f4338aa207b0203cca2f2e136a0bc9c3a444c9b4
Denys Liienko <lienko.d.k@gmail.com>
2022-08-30 15:40:41 +0300

    Added viewmodels for message events

--- BackEnd/Core/VIewModels/Messages/MessageDeleteViewModel.cs
+++ BackEnd/Core/VIewModels/Messages/MessageDeleteViewModel.cs
@@ -1,8 +1,7 @@
 namespace Core.VIewModels.Messages;
 
-public class MessageUpdateViewModel
+public class MessageDeleteViewModel
 {
     public int Id { get; set; }
     public int ChatroomId { get; set; }
-    public string? Text { get; set; }
 }

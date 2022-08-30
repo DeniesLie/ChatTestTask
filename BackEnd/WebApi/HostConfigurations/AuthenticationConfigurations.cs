b9bdfe4560ca20cb0589ddc2002fee125366705d
Denys Liienko <lienko.d.k@gmail.com>
2022-08-30 15:46:32 +0300

    Add authentication for hubs

--- BackEnd/WebApi/HostConfigurations/AuthenticationConfigurations.cs
+++ BackEnd/WebApi/HostConfigurations/AuthenticationConfigurations.cs
@@ -20,6 +20,21 @@ public static class AuthenticationConfigurations
             {
                 ClockSkew = TimeSpan.Zero
             };
+            options.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
+                    var accessToken = context.Request.Query["access_token"];
+
+                    var path = context.HttpContext.Request.Path;
+                    if (!string.IsNullOrEmpty(accessToken) &&
+                        (path.StartsWithSegments("/hubs/messages")))
+                    {
+                        context.Token = accessToken;
+                    }
+                    return Task.CompletedTask;
+                }
+            };
         });
     }
 }
